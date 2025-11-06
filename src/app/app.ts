f7370c0b0ecab324c3128c0ae5766b34e19b363f
Micheal Callaghan <mehaul96@gmail.com>
2025-11-05 22:29:29 -0500

    remove home from app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
+import { RouterOutlet } from '@angular/router'; 
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header],
+  imports: [RouterOutlet, Header],
   template: `
     <app-header />
-    <main>
-      <app-home />
-    </main>
+    <main></main>
   `,
   styles: [`
     main {
